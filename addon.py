20c07413febcccdb185578b7a75caa921958c071
Bruno Gonzalez <stenyak@stenyak.com>
2023-03-12 21:16:10 +0100

    Normalize operations from 100-based ratios to 1-based ratios

--- addon.py
+++ addon.py
@@ -45,10 +45,10 @@ class Player(xbmc.Player):
 
         return __result
 
-    # Returns Aspect ratio * 100 (i.e. 2.35 = 235)
+    # Returns Aspect ratio (i.e. 2.35)
     # Detects hardcoded black bars
     def GetAspectRatioFromFrame(self):
-        __aspectratio = int((capture.getAspectRatio() + 0.005) * 100)
+        __aspectratio = capture.getAspectRatio() + 0.005
         __threshold = 25
 
         line1 = 'Interim Aspect Ratio = ' + str(__aspectratio)
@@ -72,20 +72,20 @@ class Player(xbmc.Player):
         # Capture another frame. after we have waited for transitions
         __myimage = self.CaptureFrame()
         if self.LineColorLessThan(__myimage, 1, 5, __threshold):
-            __aspectratio = 235
+            __aspectratio = 2.35
         elif self.LineColorLessThan(__myimage, 1, 3, __threshold):
-            __aspectratio = 200
+            __aspectratio = 2.00
         elif self.LineColorLessThan(__myimage, 0, 1, __threshold):
-            __aspectratio = 185
+            __aspectratio = 1.85
 
         return __aspectratio
 
     def abolishBlackBars(self):
         aspectratio = self.GetAspectRatioFromFrame()
-        aspectratio2 = int((capture.getAspectRatio() + 0.005) * 100)
+        aspectratio2 = capture.getAspectRatio() + 0.005
         xbmc.log('Calculated Aspect Ratio = ' + str(aspectratio) + ' ' + 'Player Aspect Ratio = ' + str(aspectratio2), level=xbmc.LOGINFO)
-        if aspectratio > 178:
-            zoom_amount = aspectratio / 178
+        if aspectratio > 1.78:
+            zoom_amount = aspectratio / 1.78
             xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Player.SetViewMode", "params": {"viewmode": {"zoom": ' + str(zoom_amount) + ' }}, "id": 1}')
             notify("Zoom: {:0.2f}".format(zoom_amount))
 
